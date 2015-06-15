b56bc3b7a52cf28e81f390572c22e55e2d92d9f1
Christophe Rosset <tophe@topheman.com>
2015-06-15 15:07:28 +0200

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -116,20 +116,39 @@ gulp.task('jshint', function () {
 });
 
 /**
+ * The 'htmlhint' task defines the rules of our hinter as well as which files we
+ * should check. It helps to detect errors and potential problems in our
+ * HTML code.
+ * @todo
+ */
+gulp.task('htmlhint', function () {
+  process.stdout.write('TODO htmlhint\n');
+});
+
+/**
  * Compile SASS files into the main.css.
  * @todo
  */
 gulp.task('sass', function () {
-  console.log('TODO sass');
+  process.stdout.write('TODO sass\n');
 });
 
 /**
  * The 'watch' task set up the checks to see if any of the files listed below
  * change, and then to execute the listed tasks when they do.
- * @todo
  */
 gulp.task('watch', function () {
-  console.log('TODO watch');
+  // Watch images and fonts files
+  gulp.watch([paths.app.images, paths.app.fonts], [browserSync.reload]);
+
+  // Watch css files
+  gulp.watch(paths.app.styles, ['sass']);
+
+  // Watch js files
+  gulp.watch([paths.app.scripts, paths.gulpfile], ['jshint', browserSync.reload]);
+
+  // Watch html files
+  gulp.watch([paths.app.html, paths.app.templates], ['htmlhint', browserSync.reload]);
 });
 
 //=============================================
