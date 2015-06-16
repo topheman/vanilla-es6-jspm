249fe83e200445bc04d9906144950f53c0fd007d
Christophe Rosset <tophe@topheman.com>
2015-06-16 17:42:12 +0200

    display infos via es6 template

--- src/app/components/Geolocation/Geolocation.js
+++ src/app/components/Geolocation/Geolocation.js
@@ -1,4 +1,5 @@
 import template from './Geolocation.html!text';
+
 import geoip from '../../services/geolocation/geolocation';
 
 export default class Geolocation {
@@ -19,9 +20,18 @@ export default class Geolocation {
     }
   }
   init(){
-    this.domNode.querySelector('.geolocation-button').addEventListener('click',function(){
-      geoip().then(function(result){
+    this.domNode.querySelector('.geolocation-button').addEventListener('click',() => {
+      geoip().then((result) => {
         console.log(result);
+        let tpl = `
+          <li>City: ${result.city}</li>
+          <li>Country: ${result.country_name}</li>
+          <li>Region: ${result.region_name}</li>
+          <li>Time zone: ${result.time_zone}</li>
+          <li>Region: ${result.region_name}</li>
+          <li>Latitude : ${result.latitude} / Longitude: ${result.longitude}</li>
+        `
+        this.domNode.querySelector('.geolocation-infos').innerHTML = tpl;
       })
     }, false);
     return this;
