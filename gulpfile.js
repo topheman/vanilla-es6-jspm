6214e6b40021c2504a56227c1b4885cafe1578b9
Christophe Rosset <tophe@topheman.com>
2015-07-28 17:22:25 +0200

    refactor(gulpfile) split html related tasks to different file

--- gulpfile.js
+++ gulpfile.js
@@ -38,16 +38,6 @@ var COLORS = $.util.colors;
 //               SUB TASKS
 //=============================================
 
-/**
- * The 'htmlhint' task defines the rules of our hinter as well as which files we
- * should check. It helps to detect errors and potential problems in our
- * HTML code.
- * @todo
- */
-gulp.task('htmlhint', function () {
-  process.stdout.write('TODO htmlhint\n');
-});
-
 //=============================================
 //                MAIN TASKS
 //=============================================
