1742d70bfdebf1abb8994de0ce4bc89703ed5a04
Christophe Rosset <tophe@topheman.com>
2015-07-28 17:26:18 +0200

    refactor(gulpfile) cleanup unnecessary code

--- gulpfile.js
+++ gulpfile.js
@@ -8,43 +8,4 @@ require('babel/register');
 // require all tasks
 require('require-dir')('./gulp/tasks', { recurse: true });
 
-/**
- * Load required dependencies.
- */
-var pkg = require('./package.json');
-var del = require('del');
-var gulp = require('gulp');
-var modRewrite = require('connect-modrewrite');
-var runSequence = require('run-sequence');
-var browserSync = require('browser-sync');
-var paths = require('./gulp/paths');
-
-/**
- * Load Gulp plugins listed in 'package.json' and attaches
- * them to the `$` variable.
- */
-var $ = require('gulp-load-plugins')();
-
-//=============================================
-//            DECLARE VARIABLES
-//=============================================
-
-var log = $.util.log;
-var argv = $.util.env;
-var ENV = !!argv.env ? argv.env : 'dev';
-var COLORS = $.util.colors;
-
-//=============================================
-//               SUB TASKS
-//=============================================
-
-//=============================================
-//                MAIN TASKS
-//=============================================
-
-//---------------------------------------------
-//              DEVELOPMENT TASKS
-//---------------------------------------------
-
-
-gulp.task('default', ['serve']);
+require('gulp').task('default', ['serve']);
