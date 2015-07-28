d308f483b4637ee2a7d538c3116a3cd43510d4d9
Christophe Rosset <tophe@topheman.com>
2015-07-28 15:10:31 +0200

    refactor(gulpfile) split watch related tasks to different file

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import paths from '../paths';
+
+/**
+ * The 'watch' task set up the checks to see if any of the files listed below
+ * change, and then to execute the listed tasks when they do.
+ */
+gulp.task('watch', function () {
+  // Watch images and fonts files
+  gulp.watch([paths.app.assets.images, paths.app.assets.fonts], [browserSync.reload]);
+
+  // Watch css files
+  gulp.watch(paths.app.styles, ['sass']);
+
+  // Watch js files
+  gulp.watch([paths.app.scripts, paths.gulpfile], ['jshint', browserSync.reload]);
+
+  // Watch html files
+  gulp.watch([paths.app.html, paths.app.templates], ['htmlhint', browserSync.reload]);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -48,24 +48,6 @@ gulp.task('htmlhint', function () {
   process.stdout.write('TODO htmlhint\n');
 });
 
-/**
- * The 'watch' task set up the checks to see if any of the files listed below
- * change, and then to execute the listed tasks when they do.
- */
-gulp.task('watch', function () {
-  // Watch images and fonts files
-  gulp.watch([paths.app.assets.images, paths.app.assets.fonts], [browserSync.reload]);
-
-  // Watch css files
-  gulp.watch(paths.app.styles, ['sass']);
-
-  // Watch js files
-  gulp.watch([paths.app.scripts, paths.gulpfile], ['jshint', browserSync.reload]);
-
-  // Watch html files
-  gulp.watch([paths.app.html, paths.app.templates], ['htmlhint', browserSync.reload]);
-});
-
 //=============================================
 //                MAIN TASKS
 //=============================================
