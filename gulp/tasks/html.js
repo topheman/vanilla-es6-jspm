75cd5c3f0df2a89f37a9a7ea7525667f019864cb
Christophe Rosset <tophe@topheman.com>
2015-07-30 15:53:47 +0200

    chore(*) add htmlhint task
    Since the project contains partial html, it needs a .htmlhintrc file specifying that doctype is not
    necessary.
    The task has to be aware of this file (it doesn't discover it automatically)

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,13 +1,19 @@
 'use strict';
 
 import gulp from 'gulp';
+import htmlhint from 'gulp-htmlhint';
+import paths from '../paths';
 
 /**
  * The 'htmlhint' task defines the rules of our hinter as well as which files we
  * should check. It helps to detect errors and potential problems in our
  * HTML code.
- * @todo
+ *
+ * @return {Stream}
  */
 gulp.task('htmlhint', () => {
-  process.stdout.write('TODO htmlhint\n');
+  return gulp.src([paths.app.html, paths.app.templates])
+    .pipe(htmlhint('.htmlhintrc'))
+    .pipe(htmlhint.reporter())
+    .pipe(htmlhint.failReporter());
 });
