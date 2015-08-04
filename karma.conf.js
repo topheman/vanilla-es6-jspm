bbc0ca56ebe4121189d8abb39e177c2096a7781d
Christophe Rosset <tophe@topheman.com>
2015-08-04 16:42:08 +0200

    feat(karma) test for Spinner/Spinner.js

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,10 @@ module.exports = function (config) {
         'node_modules/phantomjs-polyfill/bind-polyfill.js',//necessary for PhantomJS (doesn't have Function.bind)
         'src/**/*.spec.js'
       ],
-      serveFiles: ['src/app/**/*.js','src/app/**/*.html']
+      serveFiles: [
+        'src/app/**/*.js',
+        'src/app/**/*.html'
+      ]
     },
 
     proxies: {
@@ -34,7 +37,7 @@ module.exports = function (config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
     browsers: ['PhantomJS'],
 
---
+++ src/app/components/Spinner/Spinner.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import Spinner from './Spinner.js';
+
+describe('components/Spinner/Spinner.js', function () {
+
+  let spinner;
+  let htmlElement;
+  let expectedInnerHTML = `<div id="circularG">
+  <div id="circularG_1" class="circularG">
+  </div>
+  <div id="circularG_2" class="circularG">
+  </div>
+  <div id="circularG_3" class="circularG">
+  </div>
+  <div id="circularG_4" class="circularG">
+  </div>
+  <div id="circularG_5" class="circularG">
+  </div>
+  <div id="circularG_6" class="circularG">
+  </div>
+  <div id="circularG_7" class="circularG">
+  </div>
+  <div id="circularG_8" class="circularG">
+  </div>
+</div>`;
+
+  beforeEach(()=>{
+    htmlElement = document.createElement('div');
+    spinner = new Spinner(htmlElement);
+  });
+
+  it('.init method should be defined', ()=>{
+    expect(spinner.init).not.toBe(undefined);
+  });
+  it('.init() should return this', () => {
+    expect(spinner.init() instanceof Spinner).toBe(true);
+  });
+  it('.domNode.innerHTML should be correct', () => {
+    expect(spinner.domNode.innerHTML).toEqual(expectedInnerHTML);
+  });
+
+});
