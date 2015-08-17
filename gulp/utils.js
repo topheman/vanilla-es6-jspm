cfa320106bf5dc2d0d8201b6160be0e0e697cc31
Christophe Rosset <tophe@topheman.com>
2015-08-17 19:28:25 +0200

    refactor(gulp/utils) remove dead code

--- gulp/utils.js
+++ gulp/utils.js
@@ -4,6 +4,3 @@ import util from 'gulp-util';
 
 export const LOG = util.log;
 export const COLORS = util.colors;
-
-const argv = util.env;
-export const ENV = !!argv.env ? argv.env.toUpperCase() : 'DEV';
