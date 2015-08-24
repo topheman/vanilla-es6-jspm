ed5bdc7e31c87594363f7173d0fff1318ee375d1
Christophe Rosset <tophe@topheman.com>
2015-08-24 15:12:10 +0200

    test(*) test e2e home page

--- protractor.config.js
+++ protractor.config.js
@@ -3,14 +3,44 @@
 require('babel/register');//write test in es6
 var SpecReporter = require('jasmine-spec-reporter');
 
+/**
+ * The default port on which the test will be run is the one specified in package.json under config.port
+ *
+ * To overload this port, just pass the flag --port
+ *
+ * Use the global goToUrl(relativeUrl) helper (which will use what ever port you defined)
+ *
+ */
+var argv = require('minimist')(process.argv.slice(2));
+var PORT = argv.port || process.env.npm_package_config_port || 9000;
+var BASE_URL = argv['base-url'] || 'http://localhost';
+console.log('[INFOS] Testing on ' + BASE_URL + ':' + PORT);
+
 var config = {
   framework: 'jasmine2',
   specs: [
     'test/e2e/spec/**/*.spec.js'
   ],
-  onPrepare: function(){
-    global.isAngularSite = function(flag){
+  onPrepare: function () {
+    /**
+     * Set on/off angular synchronisation of protractor
+     * @param flag
+     */
+    global.isAngularSite = function (flag) {
       browser.ignoreSynchronization = !flag;
+    };
+    /**
+     * Helper to use instead of directly `browser.get` so that you don't bother about the port
+     * baseUrl and port are optional and can be overriden globally when launching protractor
+     * with the flags --base-url and --port
+     * @param relativeUrl
+     * @param baseUrl
+     * @param port
+     */
+    global.goToUrl = function (relativeUrl, baseUrl, port) {
+      baseUrl = typeof baseUrl === 'undefined' ? BASE_URL : baseUrl;
+      port = typeof port === 'undefined' ? PORT : port;
+      browser.get(baseUrl + ':' + port + relativeUrl);
     };
     jasmine.getEnv().addReporter(new SpecReporter());
   }
--- test/e2e/spec/home.spec.js
+++ test/e2e/spec/home.spec.js
@@ -2,13 +2,31 @@
 
 import {beforeEachIsAngular} from '../utils.js';
 
-beforeEachIsAngular(false);
+// before each 'it' of the following describe:
+beforeEachIsAngular(false, () => {
+  //go to page (if not yet)
+  goToUrl('/');
+  //wait for the component to be loaded
+  browser.driver.wait(() => {
+    //will stop wait when the element is displayed - (~like a DOM Ready for e2e tests) - timeout is only here to error in case it takes too long
+    return browser.driver.findElement(by.id('geolocation')).isDisplayed();
+  }, 4000);
+});
 describe('Home page', () => {
-  beforeEachIsAngular(false);
-  describe('Simple checks', () => {
-    it('should have a title', () => {
-      browser.get('http://localhost:9000');
-      expect(browser.getTitle()).toEqual('vanilla-es6-jspm');
+  it('should have a title', () => {
+    expect(browser.getTitle()).toEqual('vanilla-es6-jspm');
+  });
+  it('geolocation widget should be present', () => {
+    expect(element(by.id('geolocation')).isPresent()).toBeTruthy();
+  });
+  it('results should show on "click to launch geolocation"', () => {
+    browser.findElement(by.css('button.geolocation-button')).click().then(() => {
+      expect(element(by.css('.geolocation-infos')).isDisplayed()).toBeTruthy();
+    });
+  });
+  it('results should be right on "click to launch geolocation"', () => {
+    browser.findElement(by.css('button.geolocation-button')).click().then(() => {
+      expect(element.all(by.css('.geolocation-infos li')).first().getText()).toEqual('City: Hello World !!!');
     });
   });
 });
