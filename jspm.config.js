70afed45e317a0c0ffff1a5a12b95b49bb4d8c0b
Christophe Rosset <tophe@topheman.com>
2015-09-19 21:41:06 +0200

    chore(*) update jspm to v0.16.6 & update core dependencies
    After running jspm dl-loader --latest:
    * updated babel, babel-runtime & core-js to latest
    * now running with system.js v0.19.0
    
    Up to date jspm side.

--- jspm.config.js
+++ jspm.config.js
@@ -16,11 +16,11 @@ System.config({
   },
 
   map: {
-    "babel": "npm:babel-core@5.8.22",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.24",
     "bootstrap-sass": "npm:bootstrap-sass@3.3.5",
     "clean-css": "npm:clean-css@3.3.9",
-    "core-js": "npm:core-js@1.1.1",
+    "core-js": "npm:core-js@1.1.4",
     "css": "github:systemjs/plugin-css@0.1.13",
     "fetch-polyfill": "npm:fetch-polyfill@0.8.1",
     "text": "github:systemjs/plugin-text@0.0.2",
@@ -71,7 +71,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.20": {
+    "npm:babel-runtime@5.8.24": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:buffer@3.4.2": {
@@ -100,7 +100,7 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@1.1.1": {
+    "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
