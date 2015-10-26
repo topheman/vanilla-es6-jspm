170bbe06c122238ced7b806a55bfae409ad26b39
Christophe Rosset <tophe@topheman.com>
2015-10-26 19:52:29 +0100

    chore(*) update protractor.config for saucelabs
    Saucelabs is updating Selenium & ChromeDriver to the latest version.
    So I'm updating the protractor config so that it takes in account those new versions right now (to avoid discovering the build breaks in a week ...)
    
    https://github.com/SeleniumHQ/selenium/blob/master/java/CHANGELOG
    https://sites.google.com/a/chromium.org/chromedriver/downloads

--- protractor.config.js
+++ protractor.config.js
@@ -68,6 +68,8 @@ if (process.env.TRAVIS) {
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
   config.capabilities = {
     'browserName': 'chrome',
+    'seleniumVersion': '2.48.2',
+    'chromedriverVersion': '2.20',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   };
